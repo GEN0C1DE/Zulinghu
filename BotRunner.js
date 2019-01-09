697a1416d6e4f805f52e71eafb9392e3590124e7
Christopher <firedoglo1941@gmail.com>
2019-01-09 18:56:28 -0500

    Update BotRunner.js

--- BotRunner.js
+++ BotRunner.js
@@ -1,15 +1,15 @@
 // Coded by Pharaoh#6666 | Christopher H.
 
-const Name = "TFN Bot" // The Name of the Bot, will be used in various things.
-const Version = "0.0.1 WIP" // The Version of the Bot, will be used in various things.
-const Discord = require('discord.js') // The Bots Library used to Require the Bot.
-const Commando = require('discord.js-commando') // The Bots Command Library used for Command Use.
-const Status = 'The Frosty Nation Mane.' // The Bots Status for the Playing or Streaming.
-const Testing = false // This is for the Maintenance of the Bot, wont be enabled unless working on something.
+const Name = "TFN Bot"; // The Name of the Bot, will be used in various things.
+const Version = "0.0.1 WIP"; // The Version of the Bot, will be used in various things.
+const Discord = require('discord.js'); // The Bots Library used to Require the Bot.
+const Commando = require('discord.js-commando'); // The Bots Command Library used for Command Use.
+const Status = 'The Frosty Nation Mane.'; // The Bots Status for the Playing or Streaming.
+const Testing = false; // This is for the Maintenance of the Bot, wont be enabled unless working on something.
 
 // Getting Bot Setup.
 global.Bot = new Commando.Client({
-    commandPrefix: 'tfn!"'
+    commandPrefix: 'tfn!'
 })
 
 
@@ -29,7 +29,7 @@ Bot.on("ready", function () {
     if (Testing === true) {
         Bot.user.setStatus("idle");
         Bot.user.setActivity("Maintenance Mode On, Will Be Back Soon.")
-    }    
+}    
 
 
 // Bot Login Process
