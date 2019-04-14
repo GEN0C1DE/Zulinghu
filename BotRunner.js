02b35176255f1b67f2c098f00696d86aeb31899c
Christopher <firedoglo1941@gmail.com>
2019-04-14 15:11:45 -0400

    Add files via upload

--- BotRunner.js
+++ BotRunner.js
@@ -39,7 +39,7 @@ Bot.on("message", Message => {
 	if (Message.channel.id === "529819167017402398"){
 		let PartnerRole = Message.guild.roles.find("name", "🔱 Partner Managers 🔱")
 		if (Message.member.roles.has(PartnerRole) || Message.member.hasPermission("ADMINISTRATOR")){
-			if (Message.content.includes('discord.gg/'||'discordapp.com/invite/')) {
+			if (Message.content.includes('discord.gg/') || Message.content.includes('discordapp.com/invite/')) {
 				XPNDLVL.findOne({
 					UserId: Message.author.id
 				}, (Error, Results) => {
