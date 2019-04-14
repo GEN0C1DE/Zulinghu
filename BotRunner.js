0818557eb2468b809c30d713a704ecc10ba29136
Christopher <firedoglo1941@gmail.com>
2019-04-14 14:25:58 -0400

    Add files via upload

--- BotRunner.js
+++ BotRunner.js
@@ -7,13 +7,13 @@ const Testing = false; // This is for the Maintenance of the Bot, wont be enable
 
 const Discord = require('discord.js'); // The Bots Library used to Require the Bot.
 const Mongoose = require('mongoose'); // The Bots Database Connection
-const Timeout = require('foreach-timeout') // The Bots Timeout Function
+const Timeout = require('foreach-timeout'); // The Bots Timeout Function
 
-const Bot = new Discord.Client()
+const Bot = new Discord.Client();
 
 const Connection = `mongodb://${process.env.MonUSERTOKEN}:${process.env.MonPASSTOKEN}@ds024748.mlab.com:24748/lyaboo_server`
-const Login = process.env.BOT_TOKEN
-const XPNDLVL = require(__dirname + "/structs/Schemas/levelSchema.js"),
+const Login = process.env.BOT_TOKEN;
+const XPNDLVL = require(__dirname + "/structs/Schemas/levelSchema.js");
 
 // Getting Rainbow Settings.
 const Rainbow = ["FF0D00", "FF2800", "FF3D00", "FF4F00", "FF5F00", "FF6C00", "FF7800", "FF8300", "FF8C00", "FF9500", "FF9E00", "FFA500", "FFAD00", "FFB400", "FFBB00", "FFC200", "FFC900", "FFCF00", "FFD600", "FFDD00", "FFE400", "FFEB00", "FFF200", "FFFA00", "F7FE00", "E5FB00", "D5F800", "C6F500", "B7F200", "A8F000", "98ED00", "87EA00", "74E600", "5DE100", "41DB00", "1DD300", "00C618", "00BB3F", "00B358", "00AC6B", "00A67C", "009E8E", "028E9B", "06799F", "0969A2", "0C5DA5", "0E51A7", "1047A9", "133CAC", "1531AE", "1924B1", "1F1AB2", "2A17B1", "3415B0", "3C13AF", "4512AE", "4E10AE", "560EAD", "600CAC", "6A0AAB", "7608AA", "8506A9", "9702A7", "AD009F", "BC008D", "C7007D", "D0006E", "D8005F", "DF004F", "E7003E", "EF002A", "F80012"];
